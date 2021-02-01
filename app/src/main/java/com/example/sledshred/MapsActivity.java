4ec6aa7bb2f4b58017cf18277fd3711c84ba9912
jonedwards_dot_tech <jon@jonedwards.tech>
2021-02-01 15:16:29 +0100

    Learns new tricks with 'markers'

--- app/src/main/java/com/example/sledshred/MapsActivity.java
+++ app/src/main/java/com/example/sledshred/MapsActivity.java
@@ -2,12 +2,14 @@ package com.example.sledshred;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -25,22 +27,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng m1 = new LatLng(50, 50);
+
+
+        mMap.addMarker(new MarkerOptions()
+                .position(m1)
+                .draggable(true)
+                .alpha(2)
+                .snippet("Eat a dick!")
+                // .icon(BitmapDescriptorFactory.fromResource(R.drawable.sled))
+                .title("m1"));
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(m1));
     }
 }
